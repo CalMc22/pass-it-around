84be12ad9fbe84751b5de58e957f4acc9190ffbb
Caleb Krysos <CalMc22>
2024-01-15 21:17:03 -0500

    add more fun battle royale related lyrics

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const app = express()
 const PORT = 5000
 
 app.get('/', (req, res) => {
-    res.send(`<h1>99 players in the battle royale </h1> <a href='/98'>Take one down, pass it around</a>`)
+    res.send(`<h1>99 players in the battle royale </h1> <a href='/98'>Take one out, emote on them downed</a>`)
 })
 
 app.get('/:numberOfPlayers', function(req, res) {
     if (req.params.numberOfPlayers == 0)
-        res.send(`<a href='/'>Start over.</a>`)
-    else res.send(`<h1> ${req.params.numberOfPlayers} players in the battle royale </h1> <a href='/${req.params.numberOfPlayers - 1}'>Take one down, pass it around</a>`)
+        res.send(`<h1>Victory Royale!</h1> <a href='/'>Ready up.</a>`)
+    else res.send(`<h1> ${req.params.numberOfPlayers} players in the battle royale </h1> <a href='/${req.params.numberOfPlayers - 1}'>Take one out, emote on them downed</a>`)
     })
     
 
